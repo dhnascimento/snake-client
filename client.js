2bf5008e3fda82add63b40d36b9cc618f77690c6
Daniel Nascimento <danielhn@gmail.com>
2020-07-29 20:42:05 +0000

    Added some pre-recorded messages into the script

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection( {
-    host: '10.0.2.15',
+    host:"localhost",
     port: 50541
   });
 
@@ -23,17 +23,7 @@ const connect = function() {
     conn.write('Name: DHN');
   });
 
-  // conn.on('connect', () => {
-  //   setTimeout( () => conn.write("Move: down"), 1000);
-  // });
 
-  // conn.on('connect', () => {
-  //   setTimeout( () => conn.write("Move: down"), 1500);
-  // });
-
-  // conn.on('connect', () => {
-  //   setInterval( () => conn.write("Move: left"), 2000);
-  // });
 
    return conn;
 };
--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -7,10 +11,25 @@ const setupInput = function() {
   return stdin;
 }
 
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 'd') {
+    connection.write("Move: down");
+  } else if (key === 'y') {
+    connection.write("Say: YoY");
+  } else if (key === 't') {
+    connection.write("Say: Terrific");
   }
+     
 };
+
 
 module.exports = { setupInput };
