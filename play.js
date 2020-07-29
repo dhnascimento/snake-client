b627ff10987c5363fb5985797755a414e7834551
Daniel Nascimento <danielhn@gmail.com>
2020-07-29 17:06:56 +0000

    Created a separated module for the TCP connection

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
-const net = require('net');
+// play.js
 
-/**
- * Establishes connection with the game server
- */
+const { connect } = require('./client');
+console.log('Connecting...');
 
- const connect = function() {
-   const conn = net.createConnection( {
-     host: '10.0.2.15',
-     port: 50541
-   });
-
-  // Interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-
-  return conn;
-   })
-};
-
- console.log('Connecting ...');
- 
- connect();
+connect();
